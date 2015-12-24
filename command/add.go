10527de888426c379aa57c1ccb8282698ce35ddd
kakakakakku <y.yoshida22@gmail.com>
2015-12-24 09:18:00 +0900

    Added adding task

--- command/add.go
+++ command/add.go
@@ -1,8 +1,10 @@
 package command
 
 import (
+	"database/sql"
 	"fmt"
 	"github.com/codegangsta/cli"
+	"log"
 )
 
 func CmdAdd(c *cli.Context) {
@@ -13,4 +15,15 @@ func CmdAdd(c *cli.Context) {
 
 	title := c.Args()[0]
 	fmt.Println("Added new task :", title)
+
+	db, err := sql.Open("sqlite3", "./togoo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("INSERT INTO todos(title, is_done) values('" + title + "', 0)")
+	if err != nil {
+		log.Fatal(err)
+	}
 }
