ca94510ce462570c56f67fca534bcc8fd09472c4
kakakakakku <y.yoshida22@gmail.com>
2015-12-24 13:15:35 +0900

    Added done task

--- command/done.go
+++ command/done.go
@@ -1,8 +1,29 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/codegangsta/cli"
+	"log"
+)
 
 func CmdDone(c *cli.Context) {
-	// Write your code here
 
+	if len(c.Args()) != 1 {
+		return
+	}
+
+	id := c.Args()[0]
+	fmt.Printf("Task %s is done\n", id)
+
+	db, err := sql.Open("sqlite3", "./togoo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec("UPDATE todos SET is_done = 1 WHERE id = " + id)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
