3d93cdf6b61c6c02d2fff2e4ebf9f6d1135d7473
kakakakakku <y.yoshida22@gmail.com>
2015-12-24 10:52:23 +0900

    Added listing tasks

--- command/list.go
+++ command/list.go
@@ -1,8 +1,41 @@
 package command
 
-import "github.com/codegangsta/cli"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/codegangsta/cli"
+	_ "github.com/mattn/go-sqlite3"
+	"log"
+)
 
 func CmdList(c *cli.Context) {
-	// Write your code here
+	if len(c.Args()) != 0 {
+		return
+	}
 
+	db, err := sql.Open("sqlite3", "./togoo.db")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	rows, err := db.Query("SELECT id, title, is_done FROM todos")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer rows.Close()
+
+	fmt.Printf("| %-3s | %-50s | %-7s |\n", "---", "--------------------------------------------------", "-------")
+	fmt.Printf("| %-3s | %-50s | %-7s |\n", "No", "Title", "Status")
+	fmt.Printf("| %-3s | %-50s | %-7s |\n", "---", "--------------------------------------------------", "-------")
+
+	for rows.Next() {
+		var id int
+		var title string
+		var is_done int
+		rows.Scan(&id, &title, &is_done)
+		fmt.Printf("| %3d | %-50s | %-7d |\n", id, title, is_done)
+	}
+
+	fmt.Printf("| %-3s | %-50s | %-7s |\n", "---", "--------------------------------------------------", "-------")
 }
