8d5a5a2c386f343970273deb6c373779e7a4a432
kakakakakku <y.yoshida22@gmail.com>
2016-01-03 00:56:22 +0900

    Fixed commands.go with golint

--- commands.go
+++ commands.go
@@ -8,28 +8,30 @@ import (
 	"github.com/kakakakakku/togoo/command"
 )
 
+// GlobalFlags is global option for all command.
 var GlobalFlags = []cli.Flag{}
 
-var ForceFlags = []cli.Flag{
+var forceFlags = []cli.Flag{
 	cli.BoolFlag{
 		Name:  "force, f",
 		Usage: "Force initialize even if database already existed.",
 	},
 }
 
-var AllFlags = []cli.Flag{
+var allFlags = []cli.Flag{
 	cli.BoolFlag{
 		Name:  "all, a",
 		Usage: "List all tasks.",
 	},
 }
 
+// Commands are slice of command.
 var Commands = []cli.Command{
 	{
 		Name:   "init",
 		Usage:  "",
 		Action: command.CmdInit,
-		Flags:  ForceFlags,
+		Flags:  forceFlags,
 	},
 	{
 		Name:   "add",
@@ -41,7 +43,7 @@ var Commands = []cli.Command{
 		Name:   "list",
 		Usage:  "",
 		Action: command.CmdList,
-		Flags:  AllFlags,
+		Flags:  allFlags,
 	},
 	{
 		Name:   "done",
@@ -51,6 +53,7 @@ var Commands = []cli.Command{
 	},
 }
 
+// CommandNotFound is custom error.
 func CommandNotFound(c *cli.Context, command string) {
 	fmt.Fprintf(os.Stderr, "%s: '%s' is not a %s command. See '%s --help'.", c.App.Name, command, c.App.Name, c.App.Name)
 	os.Exit(2)
